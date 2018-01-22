286bed2d5a1ce60764a79c546b5977d3f8014ce6
Heuster <32402568+Heuster@users.noreply.github.com>
2018-01-22 13:58:56 -0800

    Initialize 1/22/2018

---
+++ WordLists.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class WordLists here.
+ *
+ * @author Danny Heu
+ * @version 1.22.2018
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class WordLists
+{
+    private ArrayList mylist;
+    
+    public int numWordsOfLength(int len){
+        //
+    }
+    
+}
